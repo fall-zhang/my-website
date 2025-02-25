ee6dc112a1b0d5ae204f9338e8b61854c7b030f4
fall-zhang <zhangbofei@163.com>
2025-02-25 21:24:06 +0800

    更新 logo

--- src/pages/components/random-logo/MyLogo5.jsx
+++ src/pages/components/random-logo/MyLogo5.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import styles from './MyLogo5.module.css'
 const MyLogo = () => {
   return <>
-    <div className={styles.fame}>FALL<br></br>DESIGN <br></br><span style={{ fontSize: '6px' }}></span></div>
+    <div className={styles.fame}>FALL<br></br>ARTIST<br></br><span style={{ fontSize: '6px' }}></span></div>
   </>
 }
 export default MyLogo
--- src/pages/components/random-logo/random-logo.jsx
+++ src/pages/components/random-logo/random-logo.jsx
@@ -1,25 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import MyLogo1 from "./MyLogo1"
 import MyLogo2 from "./MyLogo2"
 import MyLogo3 from "./MyLogo3"
 import MyLogo4 from "./MyLogo4"
 import MyLogo5 from "./MyLogo5"
 export const RandomLogo = ()=>{
-  const current = Math.floor(Math.random() *5)
-  // if(current === 0){
-  //   return <MyLogo1 />
-  // }
-  // if(current === 1){
-  //   return <MyLogo2 />
-  // }
-  // if(current === 2){
-  //   return <MyLogo3 />
-  // }
-  // if(current === 3){
-  //   return <MyLogo4 />
-  // }
-  // if(current === 4){
-  //   return <MyLogo5 />
-  // }
-  return <MyLogo1 />
+  const [current,setCurrent] = useState(Math.floor(Math.random() * 5))
+  let RenderLogo  = <></>
+  if(current === 0){
+    RenderLogo = <MyLogo1 />
+  }
+  if(current === 1){
+    RenderLogo = <MyLogo2 />
+  }
+  if(current === 2){
+    RenderLogo = <MyLogo3 />
+  }
+  if(current === 3){
+    RenderLogo = <MyLogo4 />
+  }
+  if(current === 4){
+    RenderLogo = <MyLogo5 />
+  }
+  return <div className='g-cursor-pointer' onClick={()=>setCurrent(Math.floor(Math.random() *5))}>
+    {RenderLogo}
+  </div>
 }
