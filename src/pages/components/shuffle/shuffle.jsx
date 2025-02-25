f4d7e97bc76285c72ce650365e09c563cb5753aa
fall-zhang <zhangbofei@163.com>
2025-02-25 14:52:05 +0800

    添加切换动画

---
+++ src/pages/components/shuffle/shuffle.jsx
@@ -0,0 +1,19 @@
+import { useEffect, useRef } from 'react'
+import styles from './index.module.css'
+import ShuffleRandomText from './shuffle-text'
+const ShuffleText = () => {
+  const shuffleDOM = useRef<HTMLHeadingElement>(null)
+  useEffect(() => {
+    if (shuffleDOM.current) {
+      const shuffle = new ShuffleRandomText(shuffleDOM.current)
+      shuffle.start()
+    }
+  }, [])
+  return <>
+    <h2 className={styles.head} ref={shuffleDOM}>未来的前端艺术家</h2>
+  </>
+}
+
+export {
+  ShuffleText
+} 
