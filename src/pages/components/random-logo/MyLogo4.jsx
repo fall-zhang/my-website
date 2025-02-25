b6ef12aa84c00a9b248f576292179340fd65f50c
fall-zhang <zhangbofei@163.com>
2025-02-25 16:23:27 +0800

    更新 logo4

--- src/pages/components/random-logo/MyLogo4.jsx
+++ src/pages/components/random-logo/MyLogo4.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import styles from './MyLogo4.module.css'
 const MyLogo = () => {
   return <>
-    <div><h2 className={styles.name3}>Fall Zhang</h2></div>
+    <div><h2 className={styles.name3}></h2></div>
   </>
 }
 export default MyLogo
