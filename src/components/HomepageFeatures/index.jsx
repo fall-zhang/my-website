546243abaff0685023a0ab8faedf8b7af20494ec
fall-zhang <zhangbofei@163.com>
2025-02-26 11:24:54 +0800

    移除 require，更新 icon

--- src/components/HomepageFeatures/index.jsx
+++ src/components/HomepageFeatures/index.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import clsx from 'clsx'
 import Link from '@docusaurus/Link'
 import styles from './styles.module.css'
+import svg1 from '@site/static/home-page/cube-1.svg'
+import svg2 from '@site/static/home-page/circle-three.svg'
+import svg3 from '@site/static/home-page/six-point.svg'
 
 const FeatureList = [
   {
     title: '2分钟环境搭建',
     link: '/blog/2分钟环境搭建',
-    Svg: require('@site/static/home-page/cube-1.svg').default,
+    Svg: svg1,
     description: (
       <>
         像安装npm插件一样安装开发环境！ VS code 这个强大的工具都有哪些快捷键，以及开发中常用的插件。
@@ -15,17 +18,17 @@ const FeatureList = [
     )
   },
   {
-    title: 'TypeScript 声明？声明空间？',
-    link: '/blog/TypeScript声明',
-    Svg: require('@site/static/home-page/circle-three.svg').default,
+    title: '发现更多 npm 包',
+    link: '/blog/npm 的前端包',
+    Svg:svg2,
     description: (
-        '声明空间：就是声明类型和声明变量所在的不同空间。 全局模块：就是任何文件都可以访问的模块。'
+      '不用再为找不到想要 npm 依赖而发愁，实现想做的功能前，先看看这里是否有吧'
     )
   },
   {
     title: 'git 功能查找',
     link: '/blog/git功能查找',
-    Svg: require('@site/static/home-page/six-point.svg').default,
+    Svg: svg3,
     description: (
       <>
         最实用的 git 功能查找笔记 一篇文章实现快速查找绝大多数功能，功能的意义，配置文件，以及功能的查找，一篇文章全部搞定。
