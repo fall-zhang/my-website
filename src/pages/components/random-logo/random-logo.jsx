e253fa639579e4cbafbcc6afeccf0694fed764fc
fall-zhang <zhangbofei@163.com>
2025-02-26 11:24:37 +0800

    优化样式，图标不会被选中

--- src/pages/components/random-logo/random-logo.jsx
+++ src/pages/components/random-logo/random-logo.jsx
@@ -1,28 +1,30 @@
 import React, { useState } from 'react'
-import MyLogo1 from "./MyLogo1"
-import MyLogo2 from "./MyLogo2"
-import MyLogo3 from "./MyLogo3"
-import MyLogo4 from "./MyLogo4"
-import MyLogo5 from "./MyLogo5"
+import MyLogo1 from './MyLogo1'
+import MyLogo2 from './MyLogo2'
+import MyLogo3 from './MyLogo3'
+import MyLogo4 from './MyLogo4'
+import MyLogo5 from './MyLogo5'
 export const RandomLogo = ()=>{
-  const [current,setCurrent] = useState(Math.floor(Math.random() * 5))
-  let RenderLogo  = <></>
+  const [current,setCurrent] = useState(Math.floor(Math.random() * 4))
+  let RenderLogo = <></>
   if(current === 0){
     RenderLogo = <MyLogo1 />
-  }
-  if(current === 1){
+  }else if(current === 1){
     RenderLogo = <MyLogo2 />
-  }
-  if(current === 2){
+  }else if(current === 2){
     RenderLogo = <MyLogo3 />
-  }
-  if(current === 3){
+  }else if(current === 3){
     RenderLogo = <MyLogo4 />
-  }
-  if(current === 4){
+  }else if(current === 4){
     RenderLogo = <MyLogo5 />
   }
-  return <div className='g-cursor-pointer' onClick={()=>setCurrent(Math.floor(Math.random() *5))}>
+  return <div className='g-cursor-pointer g-user-select-none' onClick={()=>setCurrent((pre)=>{
+    const newState = Math.floor(Math.random() *4)
+    if(pre === newState){
+      return 4
+    }
+    return newState
+  })}>
     {RenderLogo}
   </div>
 }
