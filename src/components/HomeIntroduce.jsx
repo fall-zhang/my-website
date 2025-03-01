a4e30c1d4a4657a5caba14a32cd5ae95bda29763
fall-zhang <zhangbofei@163.com>
2025-03-01 15:40:59 +0800

    更新 src 中的所有文件为 ts

--- src/components/HomeIntroduce.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-export default HomeIntroduce = () => {
-
-  return <div>
-    <div className=''>
-      <img src="@site/svg/media-work.svg" height={512} width={512} alt="" />
-    </div>
-  </div>
-}
