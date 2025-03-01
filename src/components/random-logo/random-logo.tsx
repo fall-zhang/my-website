324d62f6bd45e2e2952df284d468acda8ef49f00
fall-zhang <zhangbofei@163.com>
2025-03-01 16:01:41 +0800

    添加更多监听的页面

--- src/components/random-logo/random-logo.tsx
+++ src/components/random-logo/random-logo.tsx
@@ -4,23 +4,23 @@ import MyLogo2 from './MyLogo2'
 import MyLogo3 from './MyLogo3'
 import MyLogo4 from './MyLogo4'
 import MyLogo5 from './MyLogo5'
-export default function RandomLogo(){
-  const [current,setCurrent] = useState(Math.floor(Math.random() * 4))
+export default function RandomLogo () {
+  const [current, setCurrent] = useState(Math.floor(Math.random() * 4))
   let RenderLogo = <></>
-  if(current === 0){
+  if (current === 0) {
     RenderLogo = <MyLogo1 />
-  }else if(current === 1){
+  } else if (current === 1) {
     RenderLogo = <MyLogo2 />
-  }else if(current === 2){
+  } else if (current === 2) {
     RenderLogo = <MyLogo3 />
-  }else if(current === 3){
+  } else if (current === 3) {
     RenderLogo = <MyLogo4 />
-  }else if(current === 4){
+  } else if (current === 4) {
     RenderLogo = <MyLogo5 />
   }
-  return <div className='g-cursor-pointer g-user-select-none' onClick={()=>setCurrent((pre)=>{
-    const newState = Math.floor(Math.random() *4)
-    if(pre === newState){
+  return <div className='g-cursor-pointer g-user-select-none' onClick={() => setCurrent((pre) => {
+    const newState = Math.floor(Math.random() * 4)
+    if (pre === newState) {
       return 4
     }
     return newState
